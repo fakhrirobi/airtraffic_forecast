782475331a813974b188b7f56b9b81f810e191bd
Fakhri Robi Aulia <fakhrirobi.fra@gmail.com>
2022-02-07 22:37:32 +0700

    adding setup.py for modularisation

--- setup.py
+++ setup.py
@@ -9,7 +9,17 @@ setup(
     package_dir={'':'airtraffic_forecast'}, 
     py_modules=[splitext(basename(path))[0] for path in glob('airtraffic_forecast/*.py')], 
     package_data= {"":["*.txt","*.ipynb",".py"]}, 
-    install_requires=[], 
+    install_requires=[
+        "statsmodels==0.13.1", 
+        "numpy==1.19.5", 
+        "plotly==5.3.1",  
+        "tqdm==4.62.2", 
+        "xgboost==1.1.1", 
+        "neuralprophet==0.3.0", 
+        "pandas==1.3.5", 
+        "joblib==1.0.0", 
+        "scikit_learn==1.0.2"
+        ], 
     extras_require ={}, 
     
     
